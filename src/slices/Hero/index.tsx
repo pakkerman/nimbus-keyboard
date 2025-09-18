2f8a824d8feb4f5bfab4ad9851ed8d37e8727442
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-09-18 14:12:20 +0800

    feat: hero text stylings

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
+import { Bounded } from "@/components/bounded";
+
 export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
 const Hero: FC<HeroProps> = ({ slice }) => {
@@ -9,10 +11,50 @@ const Hero: FC<HeroProps> = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="blue-gradient-bg relative h-dvh text-white text-shadow-black/30 text-shadow-lg"
     >
-      <PrismicRichText field={slice.primary.heading} />
-      <PrismicRichText field={slice.primary.body} />
-      {slice.primary.buy_button_text}
+      <div className="hero-scene pointer-events-none sticky top-0 h-dvh w-full">
+        {/* canvas */}
+      </div>
+      <div className="hero-content absolute inset-x-0 top-0 h-dvh">
+        <Bounded
+          fullWidth
+          className="absolute inset-x-0 top-18 md:top-24 md:left-[8vw]"
+        >
+          <PrismicRichText
+            field={slice.primary.heading}
+            components={{
+              heading1: ({ children }) => (
+                <h1 className="hero-heading font-black-slanted text-6xl leading-[0.8] uppercase sm:text-7xl lg:text-8xl">
+                  {children}
+                </h1>
+              ),
+            }}
+          />
+        </Bounded>
+        <Bounded
+          fullWidth
+          className="hero-body absolute inset-x-0 bottom-0 md:right-[8vw] md:left-auto"
+          innerClassName="flex flex-col gap-3"
+        >
+          <div className="max-w-md">
+            <PrismicRichText
+              field={slice.primary.body}
+              components={{
+                heading2: ({ children }) => (
+                  <h2 className="mb-1 font-bold-slanted text-4xl uppercase lg:mb-2 lg:text-6xl">
+                    {children}
+                  </h2>
+                ),
+              }}
+            />
+          </div>{" "}
+        </Bounded>
+        <button className="group flex w-fit cursor-pointer items-center gap-1 rounded bg-[#01A7E1] px-3 py-1 font-bold-slanted text-2xl uppercase transition disabled:grayscale">
+          {slice.primary.buy_button_text}
+          <span className="transition group-hover:translate-x-1">{">"}</span>
+        </button>
+      </div>
     </section>
   );
 };
