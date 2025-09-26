1e994eacb841e8981c602ef3aa92dbc3f0a6bb77
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-09-26 14:01:23 +0800

    feat: animate hero text

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,16 +1,49 @@
 "use client";
 
 import { FC } from "react";
-import { Content } from "@prismicio/client";
+import { Canvas } from "@react-three/fiber";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
+
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { Content } from "@prismicio/client";
 
 import Bounded from "@/components/Bounded";
-import { Canvas } from "@react-three/fiber";
 import Scene from "./Scene";
+
+gsap.registerPlugin(useGSAP, SplitText);
 
 export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
 const Hero: FC<HeroProps> = ({ slice }) => {
+  useGSAP(() => {
+    const mm = gsap.matchMedia();
+    mm.add("(prefers-reduced-motion: no-preference)", () => {
+      const split = SplitText.create(".hero-heading", {
+        type: "chars,lines",
+        mask: "lines",
+        linesClass: "line++",
+      });
+
+      const tl = gsap.timeline({ delay: 4.2 });
+      tl.from(split.chars, {
+        opacity: 0,
+        y: -120,
+        ease: "back",
+        duration: 0.4,
+        stagger: 0.07,
+      }).to(".hero-body", {
+        opacity: 1,
+        duration: 0.6,
+        ease: "power2.out",
+      });
+    });
+    mm.add("(prefers-reduced-motion: reduced)", () => {
+      gsap.set(".hero-heading, .hero-body", { opacity: 1 });
+    });
+  });
+
   return (
     <section
       data-slice-type={slice.slice_type}
@@ -40,7 +73,7 @@ const Hero: FC<HeroProps> = ({ slice }) => {
         </Bounded>
         <Bounded
           fullWidth
-          className="hero-body absolute inset-x-0 bottom-0 md:right-[8vw] md:left-auto"
+          className="hero-body absolute inset-x-0 bottom-0 md:right-[8vw] md:left-auto opacity-0"
           innerClassName="flex flex-col gap-3"
         >
           <div className="max-w-md">
